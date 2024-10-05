0615aab6700ad5d8eff4df22fcb600c249692d73
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-10-05 07:27:47 -0500

    Clase 5 de octubre

---
+++ Practica/pagina3/app.js
@@ -0,0 +1,10 @@
+const tituloInventario = document.getElementById("tituloInventario");
+const elementoInventario = document.querySelector(".inventario");
+const botonAgregar = document.getElementById("agregarProducto");
+const botonEjecutar = document.getElementById("ejecutar");
+
+let inventario = [];
+
+function agregarProducto(){
+    elementoInventario.innerHTML = "";
+}
